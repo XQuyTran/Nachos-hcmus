3aeea4da2e78ede4677ba5ff5356fec55efbd06e
XQuyTran <55830692+XQuyTran@users.noreply.github.com>
2020-12-03 21:38:04 +0700

    update threads

--- nachos/nachos-3.4/code/threads/system.cc
+++ nachos/nachos-3.4/code/threads/system.cc
@@ -29,7 +29,7 @@ SynchDisk   *synchDisk;
 
 #ifdef USER_PROGRAM	// requires either FILESYS or FILESYS_STUB
 Machine *machine;	// user program memory and registers
-SynchConsole *gSynchConsole;
+SynchConsole* gSynchConsole;
 #endif
 
 #ifdef NETWORK
@@ -150,7 +150,6 @@ Initialize(int argc, char **argv)
     
 #ifdef USER_PROGRAM
     machine = new Machine(debugUserProg);	// this must come first
-    gSynchConsole = new SynchConsole();
 #endif
 
 #ifdef FILESYS
@@ -180,7 +179,6 @@ Cleanup()
     
 #ifdef USER_PROGRAM
     delete machine;
-    delete gSynchConsole;
 #endif
 
 #ifdef FILESYS_NEEDED
