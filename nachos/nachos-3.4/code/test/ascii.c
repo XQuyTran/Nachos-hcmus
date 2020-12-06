2f111611f2551ef17b9e433a574a647fc6ef5c7d
XQuyTran <55830692+XQuyTran@users.noreply.github.com>
2020-12-06 22:57:51 +0700

    Add files via upload

--- nachos/nachos-3.4/code/test/ascii.c
+++ nachos/nachos-3.4/code/test/ascii.c
@@ -4,18 +4,20 @@
 int main()
 {
 	int i;
-	char ch;
+	//char ch = (char)32;
 	// in tieu de
+	PrintString("ASCII Table\n");
 	PrintString("Code\tChar\n");
 	
 	// in cac ky tu in duoc, bao gom cac ky tu thuoc bang mo rong
-	for (i = 32; i < 256; i++)
+	for (i = 32; i < 127; i++)
 	{
 		PrintInt(i);
 		PrintChar('\t');
-		ch = i;
-		PrintChar(ch);
+
+		PrintChar((char)i);
 		PrintChar('\n');
+		//ch++;
 	}
 	
 	Halt();
